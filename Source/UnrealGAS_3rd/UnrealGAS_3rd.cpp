c16a882c6c2d3d900a656fc41c38eee75f9aa57a
sdchjjj <sdchjjj@gmail.com>
2026-01-05 10:49:56 +0900

    어빌리티 시스템 컴포넌트 추가

---
+++ Source/UnrealGAS_3rd/UnrealGAS_3rd.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "UnrealGAS_3rd.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UnrealGAS_3rd, "UnrealGAS_3rd" );
