ee8e230c1a71b135e46fe6c04a5d27f098081666
sdchjjj <sdchjjj@gmail.com>
2026-01-05 11:11:28 +0900

    어트리뷰트 셋 추가

---
+++ Source/UnrealGAS_3rd/Private/GameAbilitySet/StatusAttributeSet.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GameAbilitySet/StatusAttributeSet.h"
+
+UStatusAttributeSet::UStatusAttributeSet()
+{
+	InitHealth(100.0f);
+}
